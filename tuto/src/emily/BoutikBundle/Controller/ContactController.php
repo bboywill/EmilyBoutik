1d97e7fa5c0b3df88082fdd0d085f7b7370afe70
bboywill <wdacunha77@gmail.com>
2013-01-23 01:01:50 +0100

    Ajout de l'envoie d'email au formulaire de contact

--- tuto/src/emily/BoutikBundle/Controller/ContactController.php
+++ tuto/src/emily/BoutikBundle/Controller/ContactController.php
@@ -6,11 +6,19 @@ use emily\BoutikBundle\Core\Model\ContactResponse;
 
 class ContactController extends AbstractController {
     public function showAction() {
-        $form = $this->createForm(new ContactForm(), new ContactResponse());
+        $formResponse = new ContactResponse();
+		$form = $this->createForm(new ContactForm(), $formResponse);
         $request = $this->getRequest();
         if($request->getMethod() == "POST") {
             $form->bindRequest($request);
-            if($form->isValid()) {
+            if($form->isValid()) {			
+                $message = \Swift_Message::newInstance();
+                $message->setSubject("Contact");
+                $message->setFrom($this->container->getParameter("emily.contact.email_from"));
+                $message->setTo($this->container->getParameter("emily.contact.email_to"));
+				$message->setBody($this->renderView("emilyBoutikBundle:emails:contact.txt.twig", array("response" => $formResponse)));
+                $this->get("mailer")->send($message);
+                $this->get("session")->setFlash("emily_contact", "Your contact enquiry was successfully sent. Thank you!");
                 return $this->redirect($this->generateUrl("emily_contact"));
             }
         }
